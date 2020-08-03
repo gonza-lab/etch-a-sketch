e0e4493cbbec95a8d58a0ab7cfc7e25fdb9743ef
g0nza <gonzaflores358@gmail.com>
2020-08-03 12:00:11 -0300

    Add color picker

--- script.js
+++ script.js
@@ -68,6 +68,51 @@ function clearAllBlocks() {
   }
 }
 
+function createColorPicker() {
+  const pickr = Pickr.create({
+    el: ".color-picker",
+    theme: "nano", // or 'monolith', or 'nano'
+
+    swatches: [
+      "rgba(244, 67, 54, 1)",
+      "rgba(233, 30, 99, 0.95)",
+      "rgba(156, 39, 176, 0.9)",
+      "rgba(103, 58, 183, 0.85)",
+      "rgba(63, 81, 181, 0.8)",
+      "rgba(33, 150, 243, 0.75)",
+      "rgba(3, 169, 244, 0.7)",
+      "rgba(0, 188, 212, 0.7)",
+      "rgba(0, 150, 136, 0.75)",
+      "rgba(76, 175, 80, 0.8)",
+      "rgba(139, 195, 74, 0.85)",
+      "rgba(205, 220, 57, 0.9)",
+      "rgba(255, 235, 59, 0.95)",
+      "rgba(255, 193, 7, 1)",
+    ],
+
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+        hex: true,
+        rgba: true,
+        hsla: true,
+        hsva: true,
+        cmyk: true,
+        input: true,
+        clear: true,
+        save: true,
+      },
+    },
+  });
+
+  return pickr;
+}
+
 let color = "000000";
 let mode = "simple";
 let show = true;
@@ -77,6 +122,7 @@ const btnRandomColor = document.getElementById("randomColor");
 const btnColor = document.getElementById("color");
 const btnGrid = document.getElementById("grid");
 const btnClear = document.getElementById("clear");
+const pickr = createColorPicker();
 
 btnResize.addEventListener("click", () => {
   removeAllBlocks();
@@ -91,10 +137,7 @@ btnRandomColor.addEventListener("click", () => {
 });
 
 btnColor.addEventListener("click", (event) => {
-  color = aleatorio(1, 16777215).toString(16);
-  mode = "simple";
-
-  changeColor(mode, color);
+  pickr.show();
 });
 
 btnGrid.addEventListener("click", () => {
@@ -105,6 +148,13 @@ btnGrid.addEventListener("click", () => {
 btnClear.addEventListener("click", () => {
   clearAllBlocks();
 });
+
+pickr.on("save", (colorPicked) => {
+  color  = colorPicked.toHEXA()[0] + colorPicked.toHEXA()[1] + colorPicked.toHEXA()[2];
+  changeColor("simple", color);
+});
+
+
 
 addBlock(10);
 changeColor("simple", color);
